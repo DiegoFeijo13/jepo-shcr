874ae9b52710f3f3cdd21f70d9bcf8739a76d55e
Diego Feijó <diego.feijo13@gmail.com>
2022-09-03 12:34:12 -0300

    Downgrade to Unity 2021 LTS

---
+++ Assets/Scripts/Objects/Interactable/InteractableTorch.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class InteractableTorch : InteractableBase
+{  
+    [SerializeField] private bool isLit;
+    [SerializeField] private Sprite litSprite;
+    [SerializeField] private Sprite unlitSprite;
+
+
+    public GameObject LightEffect;
+
+    private void Awake()
+    {
+        LightEffect.SetActive(isLit);
+        UpdateSprite();
+    }
+
+    private void UpdateSprite()
+    {
+        if (isLit && litSprite != null)
+            spriteRenderer.sprite = litSprite;
+
+        if (!isLit && unlitSprite != null)
+            spriteRenderer.sprite = unlitSprite;
+    }
+
+    void Update()
+    {
+        UpdateInteract();
+        
+    }
+
+    public override void InteractInternal()
+    {
+        isLit = !isLit;        
+        LightEffect.SetActive(isLit);
+        UpdateSprite();
+    }
+}
