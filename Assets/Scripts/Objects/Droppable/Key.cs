4a2dceadd96ac55a84a20d46c76d989b9ab5b933
Diego Feijó <diego.feijo13@gmail.com>
2022-09-13 22:26:19 -0300

    Droppable Items now can be attracted to Ed

--- Assets/Scripts/Objects/Droppable/Key.cs
+++ Assets/Scripts/Objects/Droppable/Key.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Key : DroppableItem
 {
-    public Inventory PlayerInventory;    
+    [SerializeField] private Inventory PlayerInventory;    
 
     private void OnTriggerEnter2D(Collider2D other)
     {
